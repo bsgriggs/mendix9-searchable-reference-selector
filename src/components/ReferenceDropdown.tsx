80ee53559b419b1b1b3c14d4398e1f439579b76a
bsgriggs <benjaminsgriggs@gmail.com>
2022-11-29 14:41:28 -0500

    formatting

--- src/components/ReferenceDropdown.tsx
+++ src/components/ReferenceDropdown.tsx
@@ -153,7 +153,6 @@ const ReferenceDropdown = ({
                                 searchInput,
                                 setShowMenu
                             );
-                            
                         }}
                         title={"Clear"}
                         mxIconOverride={clearIcon}
