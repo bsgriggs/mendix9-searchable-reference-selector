ecd5243052323f7e3365464cff59fc0a06db92ee
bsgriggs <benjaminsgriggs@gmail.com>
2023-07-05 15:22:13 -0400

    media init

--- src/components/Alert.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent, createElement, ReactElement } from "react";
-export interface AlertProps {
-    alertStyle?: "default" | "primary" | "success" | "info" | "warning" | "danger";
-    className?: string;
-}
-export const Alert: FunctionComponent<AlertProps> = ({ alertStyle, children }): ReactElement | null =>
-    children ? <div className={`alert alert-${alertStyle} mx-validation-message`}>{children}</div> : null;
-Alert.displayName = "Alert";
-Alert.defaultProps = { alertStyle: "danger" };
